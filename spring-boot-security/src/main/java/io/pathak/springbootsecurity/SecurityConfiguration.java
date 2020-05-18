c1691daf00a798f24eeac24f399e301acb2ed2b1
Shivam <shivam.s.pathak@oracle.com>
2020-05-18 14:09:50 +0530

    Spring Security Authorization
    Override configure method for implementing Authorization.
    - Added 2 APIs for User and Admin

--- spring-boot-security/src/main/java/io/pathak/springbootsecurity/SecurityConfiguration.java
+++ spring-boot-security/src/main/java/io/pathak/springbootsecurity/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package io.pathak.springbootsecurity;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -18,6 +19,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 			.withUser("admin").password("admin1@3").roles("ADMIN");
 	}
 	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers("/security/admin").hasRole("ADMIN")
+			.antMatchers("/security/user").hasAnyRole("USER")
+			.and().formLogin();
+	}
+
 	@Bean
 	public PasswordEncoder getPasswordEncoder() {
 		return NoOpPasswordEncoder.getInstance();
--- spring-boot-security/src/main/java/io/pathak/springbootsecurity/resource/SpringSecurityResource.java
+++ spring-boot-security/src/main/java/io/pathak/springbootsecurity/resource/SpringSecurityResource.java
@@ -11,4 +11,14 @@ public class SpringSecurityResource {
 	public String home() {
 		return "<h1>Welcome</h1>";
 	}
+	
+	@RequestMapping("/user")
+	public String user() {
+		return "<h1>Welcome User</h1>";
+	}
+	
+	@RequestMapping("/admin")
+	public String admin() {
+		return "<h1>Welcome Admin</h1>";
+	}
 }
